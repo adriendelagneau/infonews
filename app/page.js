2506a78812caa45574720e19798a4676bde74f40
adriendelagneau <delagneauadrien@yahoo.fr>
2023-09-13 14:33:56 +0200

    first commit

--- app/page.js
+++ app/page.js
@@ -1,11 +1,22 @@
 
 
 
+async function getData() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/articles`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
 
 export default async function Home() {
- 
+  const data = await getData();
 
-
+  console.log(data)
 
 
   return (
