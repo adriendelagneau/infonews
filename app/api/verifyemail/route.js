7fafb169b3f10c2792f79b86ed8d52c29cde05f0
adriendelagneau <delagneauadrien@yahoo.fr>
2023-09-13 18:28:08 +0200

    first commit

--- app/api/verifyemail/route.js
+++ app/api/verifyemail/route.js
@@ -1,42 +1,33 @@
-import { NextResponse } from "next/server"
-import connect from "@/lib/db";
-import User from "@/models/User";
-
-
 export const POST = async (request) => {
-  
   await connect();
 
-    try {
-        const reqBody = await request.json()
-        const { token } = reqBody
+  try {
+    const reqBody = await request.json();
+    const { token } = reqBody;
 
-      console.log(typeof (token), token)
-      
-      let user
-      try {
-        user = await User.findOne({ verifyToken: token })
-        console.log(user)
-        
-      } catch (err) {
-        console.log("here", err)
-      }
-      
-      if (!user) {
-          return NextResponsejson({error: "invalid token"}, { status: 400 });
-      }
-        console.log("titi", user)
-     
+    console.log("Received token:", typeof token, token);
 
-        user.isVerified = true
-        user.verifyToken = undefined
-        user.verifyTokenExpires = undefined
+    const user = await User.findOne({
+      verifyToken: token,
+      verifyTokenExpires: { $gt: Date.now() },
+    });
 
-        await user.save()
-
-        return NextResponse.json({message: "Email verified successfully", success: true});
-    
-    } catch (err) {
-      return  NextResponse.json({error: err}, { status: 500 });
+    if (!user) {
+      console.error("User not found for token:", token);
+      return NextResponse.json({ error: "invalid token" }, { status: 400 });
     }
-  };
+
+    user.isVerified = true;
+    user.verifyToken = undefined;
+    user.verifyTokenExpires = undefined;
+
+    await user.save();
+
+    console.log("User verified:", user.email);
+
+    return NextResponse.json({ message: "Email verified successfully", success: true });
+  } catch (err) {
+    console.error("Error while processing the request:", err);
+    return NextResponse.json({ error: err }, { status: 500 });
+  }
+};
